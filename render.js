3d839c96cbeac3a8de120188e51321a0e8babfa0
BulbaSprout <pasabozenko3@gmail.com>
2025-10-12 22:02:22 +0300

    make evil app open animation!!1!

--- render.js
+++ render.js
@@ -6,7 +6,11 @@ var ctx = canvas.getContext("2d")
 var width = window.innerWidth
 var height = window.innerHeight
 var renWidth = height / 16 * 9
-var mult = 4
+var mult = 3
+var appAnim = false
+var animationSize = 0
+var appId
+var inApp = false
 const pageIcons = 4
 const fps = 60
 const img = new Image
@@ -40,7 +44,7 @@ setInterval(() => {
      
     for(let y = pageIcons + 2; y > 0; y--){
         for (let x = pageIcons; x > 0; x--) {
-            ctx.roundRect((iconSize*x)/1.05-iconSize*1+(renWidth/(mult*pageIcons)), (iconSize*y)/1.05-iconSize*1.02+(renWidth/(mult*pageIcons)), iconSize-renWidth/(mult*pageIcons), iconSize-renWidth/(mult*pageIcons), 15);
+            ctx.roundRect((iconSize*x)/1.07-iconSize*1+(renWidth/(mult*pageIcons)), (iconSize*y)/1.07-iconSize*1.02+(renWidth/(mult*pageIcons)), iconSize-renWidth/(mult*pageIcons), iconSize-renWidth/(mult*pageIcons), 15);
         }
     }
 
@@ -52,24 +56,62 @@ setInterval(() => {
     ctx.fill()
     ctx.stroke()
 
+    if (appAnim) {
+        console.log(appId)
+        ctx.beginPath()
+        if (appId < 100){
+            ctx.roundRect(elements[appId].left - animationSize / 2, elements[appId - 1].top - animationSize / 2, elements[appId].width + animationSize, elements[appId].height + animationSize, 1000); 
+        }
+        ctx.fillStyle = '#232323ff'
+        ctx.strokeStyle = '#0d0d0dff'
+        ctx.lineWidth = 30
+        ctx.fill()
+        ctx.stroke()
+        animationSize += 60
+    }
+
 }, 1000 / fps);
 
 elements.push({
     width: 30,
     height: 30,
     top: height - 35-15,
-    left: renWidth / 2-15
+    left: renWidth / 2-15,
+    type: "home"
 })
+var iconSize = renWidth/pageIcons
+for (let x = 0; x < pageIcons; x++) {
+    for (let y = 0; y < pageIcons+2; y++) {
+        elements.push({
+            width: iconSize - renWidth / (mult * pageIcons),
+            height: iconSize - renWidth / (mult * pageIcons),
+            top: (iconSize * x) / 1.05 - iconSize * 1 + (renWidth / (mult * pageIcons)),
+            left: (iconSize * y) / 1.05 - iconSize * 1.02 + (renWidth / (mult * pageIcons)),
+            type: "icon"
+        })
+    }
+}
 
+function openApp(id) {
+    appId = id-1
+    animationSize = 0
+    appAnim = true
+}
 
 canvas.addEventListener('click', function (event) {
     var x = event.pageX - canvasLeft
     var y = event.pageY - canvasTop
+    var i = 1
     elements.forEach(function (element) {
+        console.log("HI")
         if (y > element.top && y < element.top + element.height
              && x > element.left && x < element.left + element.width
         ) {
-            alert("Home button clciked")
+            if (element.type == "home"){
+            }else if (element.type == "icon"){
+                openApp(i)
+            }
         }
+        i++
     })
 })
