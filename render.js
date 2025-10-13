dd100db6c8055d59446f9ec02ec4643de1431f78
BulbaSprout <pasabozenko3@gmail.com>
2025-10-13 18:17:17 +0300

    fix click detection and app opening when other one is opened and uhh fix 4-6 rows of apps not working!

--- render.js
+++ render.js
@@ -16,7 +16,7 @@ const fps = 60
 const img = new Image
 ctx.imageSmoothingEnabled = false
 
-img.src = "System/wallpapers/wallpaper_1.jfif"
+img.src = "System/wallpapers/wallpaper_2.jfif"
 
 //RENDER
 setInterval(() => {
@@ -37,14 +37,15 @@ setInterval(() => {
     for(let y = pageIcons + 2; y > 0; y--){
         for (let x = pageIcons; x > 0; x--) {
             ctx.roundRect((iconSize*x)/1.07-iconSize*1+(renWidth/(mult*pageIcons)), (iconSize*y)/1.07-iconSize*1.02+(renWidth/(mult*pageIcons)), iconSize-renWidth/(mult*pageIcons), iconSize-renWidth/(mult*pageIcons), 15);
+            ctx.fillStyle = '#edd156ff'
+            ctx.strokeStyle = '#eeae5bff'
         }
     }
+    
+         ctx.lineWidth = 3
+         ctx.fill()
+         ctx.stroke()
 
-    ctx.fillStyle = '#eed35bff'
-    ctx.strokeStyle = '#eeae5bff'
-    ctx.lineWidth = 3
-    ctx.fill()
-    ctx.stroke()
 
     //Open animation
     if (appAnim==true){
@@ -64,7 +65,6 @@ setInterval(() => {
 
     //Close animation
     if(appAnim === "backwards"){
-        console.log("Hello world!")
         ctx.beginPath()
         if (appId < 100 && animationSize>0){
             ctx.roundRect(elements[appId].left - animationSize / 2, elements[appId - 1].top - animationSize / 2, elements[appId].width + animationSize, elements[appId].height + animationSize, 1000); 
@@ -75,7 +75,6 @@ setInterval(() => {
         ctx.fill()
         ctx.stroke()
         if (animationSize > 0){
-            console.log("ASD", animationSize)
             animationSize -= 100
         }
         if (animationSize < 1){
@@ -83,10 +82,13 @@ setInterval(() => {
             inApp = false
     }
     }
+    ctx.fillStyle = "#34343487"
+    ctx.fillRect(-renWidth/2, height-50, renWidth*2,100)
+    ctx.fill
 
     //Home button
     ctx.beginPath();
-    ctx.arc(renWidth / 2, height - 35, 10, 0, 2 * Math.PI);
+    ctx.arc(renWidth / 2, height - 25, 10, 0, 2 * Math.PI);
     ctx.lineWidth = 15;
     ctx.strokeStyle = '#000000ff';
     ctx.stroke();
@@ -98,27 +100,29 @@ setInterval(() => {
 elements.push({
     width: 30,
     height: 30,
-    top: height - 35-15,
+    top: height - 25-15,
     left: renWidth / 2-15,
     type: "home"
 })
 var iconSize = renWidth/pageIcons
-for (let x = 0; x < pageIcons; x++) {
-    for (let y = 0; y < pageIcons+2; y++) {
+for (let x = 0; x < pageIcons+3; x++) {
+    for (let y = 0; y < pageIcons+1; y++) {
         elements.push({
             width: iconSize - renWidth / (mult * pageIcons),
             height: iconSize - renWidth / (mult * pageIcons),
-            top: (iconSize * x) / 1.05 - iconSize * 1 + (renWidth / (mult * pageIcons)),
-            left: (iconSize * y) / 1.05 - iconSize * 1.02 + (renWidth / (mult * pageIcons)),
+            top: (iconSize * x) / 1.07 - iconSize * 1 + (renWidth / (mult * pageIcons)),
+            left: (iconSize * y) / 1.07 - iconSize * 1.02 + (renWidth / (mult * pageIcons)),
             type: "icon"
         })
     }
 }
-
 function openApp(id) {
-    appId = id-1
-    animationSize = 0
-    appAnim = true
+    if (!inApp){
+        inApp = true
+        appId = id-1
+        animationSize = 0
+        appAnim = true
+    }
 }
 
 function closeApp(){
@@ -130,6 +134,7 @@ canvas.addEventListener('click', function (event) {
     var y = event.pageY - canvasTop
     var i = 1
     elements.forEach(function (element) {
+        console.log("ok")
         if (y > element.top && y < element.top + element.height
              && x > element.left && x < element.left + element.width
         ) {
