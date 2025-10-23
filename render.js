b45dbaa55b704ea43a829d3e712e865126ee83f0
BulbaSprout <pasabozenko3@gmail.com>
2025-10-23 16:37:24 +0300

    fixes and cool stuff, i guess

--- render.js
+++ render.js
@@ -20,29 +20,29 @@ const fps = 60
 const img = new Image
 ctx.imageSmoothingEnabled = false
 
-function getConfig(parameter){
-fetch('System/sysConfig.br')
-  .then(response => {
-    if (!response.ok) throw new Error("Failed to load config!")
-    return response.json();
-  })
-  .then(data => {
-    img.src = data["wallpaper"]
-    console.log("Key:", data[parameter]);
-    return data[parameter]
-  })
-  .catch(err => {
-    console.error(err.message);
-    return null
-  });
-  return false
+function getConfig(parameter) {
+    fetch('System/sysConfig.br')
+        .then(response => {
+            if (!response.ok) throw new Error("Failed to load config!")
+            return response.json();
+        })
+        .then(data => {
+            img.src = data["wallpaper"]
+            console.log("Key:", data[parameter]);
+            return data[parameter]
+        })
+        .catch(err => {
+            console.error(err.message);
+            return null
+        });
+    return false
 
 }
 
-console.log("Paper of wall is", getConfig("wallpaper"))
-
+getConfig()
+requestAnimationFrame(drawIt)
 //RENDER
-setInterval(() => {
+function drawIt() {
     //Wallpaper
     height = window.innerHeight
     width = window.innerWidth
@@ -53,39 +53,46 @@ setInterval(() => {
     var iconSize = renWidth / pageIcons
 
     //Icons
-    ctx.beginPath()
 
     let x = 0
     let y = 1
+    let i = 0
+    ctx.lineWidth = height / 231
+    //if (!inApp){
     icons.forEach(function () {
         x++
-        if (x > pageIcons){
+        if (x > pageIcons) {
             x = 1
             y++
         }
-        ctx.roundRect((iconSize * x) / 1.07 - iconSize * 1 + (renWidth / (mult * pageIcons)), (iconSize * y) / 1.07 - iconSize * 1.02 + (renWidth / (mult * pageIcons)), iconSize - renWidth / (mult * pageIcons), iconSize - renWidth / (mult * pageIcons), height/46);
-        ctx.fillStyle = '#edd156ff'
-        ctx.strokeStyle = '#eeae5bff'
+        ctx.fillStyle = icons[i].fc
+        ctx.strokeStyle = icons[i].sc
+        ctx.beginPath()
+        ctx.roundRect(icons[i].left, icons[i].top, iconSize - renWidth / (mult * pageIcons), iconSize - renWidth / (mult * pageIcons), height / 46);
+        ctx.fill()
+        ctx.stroke()
+
+        i++
     })
 
-    ctx.lineWidth = height/231
-    ctx.fill()
-    ctx.stroke()
+    //}
+
+
 
 
     //Open animation
     if (appAnim == true || inApp == true && !appAnim == "backwards") {
         ctx.beginPath()
         if (appId < 100) {
-            ctx.roundRect(elements[appId].left - animationSize / 2, elements[appId].top - animationSize / 2, elements[appId].width + animationSize, elements[appId].height + animationSize, 1000);
+            ctx.roundRect(elements[appId].left - animationSize / 2, elements[appId].top - animationSize / 2, elements[appId].width + animationSize, elements[appId].height + animationSize, height / 5);
         }
         ctx.fillStyle = '#232323ff'
         ctx.strokeStyle = '#0d0d0dff'
-        ctx.lineWidth = height/23
+        ctx.lineWidth = height / 23
         ctx.fill()
         ctx.stroke()
         if (animationSize < height * 2) {
-            animationSize += height/11.6
+            animationSize += height / 11.6
         }
     }
 
@@ -93,15 +100,15 @@ setInterval(() => {
     if (appAnim === "backwards") {
         ctx.beginPath()
         if (appId < 100 && animationSize > 0) {
-            ctx.roundRect(elements[appId].left - animationSize / 2, elements[appId].top - animationSize / 2, elements[appId].width + animationSize, elements[appId].height + animationSize, 1000);
+            ctx.roundRect(elements[appId].left - animationSize / 2, elements[appId].top - animationSize / 2, elements[appId].width + animationSize, elements[appId].height + animationSize, height / 5);
         }
         ctx.fillStyle = '#232323ff'
         ctx.strokeStyle = '#0d0d0dff'
-        ctx.lineWidth = height/23
+        ctx.lineWidth = height / 23
         ctx.fill()
         ctx.stroke()
         if (animationSize > 0) {
-            animationSize -= height/6.9
+            animationSize -= height / 6.9
         }
         if (animationSize < 1) {
             appAnim = false
@@ -111,16 +118,8 @@ setInterval(() => {
     }
 
     //debugging
-    if (isDev){
-        var iconSize = renWidth / pageIcons
-        for (let x = 0; x < pageIcons; x++) {
-            for (let y = 0; y < pageIcons + 2; y++) {
-                ctx.fillRect(iconSize - renWidth / (mult * pageIcons),iconSize - renWidth / (mult * pageIcons),(iconSize * ((iconAmount % pageIcons)+1)) / 1.07 - iconSize * 1 + (renWidth / (mult * pageIcons)),(iconSize * ((Math.floor(iconAmount / pageIcons))+1)) / 1.07 - iconSize * 1.02 + (renWidth / (mult * pageIcons)))
-                ctx.fillStyle = '#ff0000ff'
-                ctx.strokeStyle = '#000000ff'   
-    }
-        }
-    }
+
+
 
     //Remote Drawing
     function createRectAt(x, y, w, h, c, s, l, f) {
@@ -135,19 +134,21 @@ setInterval(() => {
     //createRectAt(100, 100, 100, 100, "#eb4034", "#5634ebff",10,true)
 
     ctx.fillStyle = "#34343487"
-    ctx.fillRect(-renWidth / 2, height - height/14, renWidth * 2, 100)
+    ctx.fillRect(-renWidth / 2, height - height / 14, renWidth * 2, 100)
     ctx.fill
 
     //Home button
     ctx.beginPath();
-    ctx.arc(renWidth / 2, height - height/28, height/69.5, 0, 2 * Math.PI);
-    ctx.lineWidth = height/46;
+    ctx.arc(renWidth / 2, height - height / 28, height / 69.5, 0, 2 * Math.PI);
+    ctx.lineWidth = height / 46;
     ctx.strokeStyle = '#000000ff';
     ctx.stroke();
-    ctx.lineWidth = height/87;
+    ctx.lineWidth = height / 87;
     ctx.strokeStyle = '#ffffffff';
     ctx.stroke();
-}, 1000 / fps);
+
+    requestAnimationFrame(() => drawIt());
+};
 
 elements.push({
     width: 30,
@@ -159,13 +160,13 @@ elements.push({
 
 var iconSize = renWidth / pageIcons
 
-addIcon(iconSize - renWidth / (mult * pageIcons),(iconSize * ((iconAmount % pageIcons)+1)) / 1.07 - iconSize * 1 + (renWidth / (mult * pageIcons)),(iconSize * ((Math.floor(iconAmount / pageIcons))+1)) / 1.07 - iconSize * 1.02 + (renWidth / (mult * pageIcons)))
-addIcon(iconSize - renWidth / (mult * pageIcons),(iconSize * ((iconAmount % pageIcons)+1)) / 1.07 - iconSize * 1 + (renWidth / (mult * pageIcons)),(iconSize * ((Math.floor(iconAmount / pageIcons))+1)) / 1.07 - iconSize * 1.02 + (renWidth / (mult * pageIcons)))
-addIcon(iconSize - renWidth / (mult * pageIcons),(iconSize * ((iconAmount % pageIcons)+1)) / 1.07 - iconSize * 1 + (renWidth / (mult * pageIcons)),(iconSize * ((Math.floor(iconAmount / pageIcons))+1)) / 1.07 - iconSize * 1.02 + (renWidth / (mult * pageIcons)))
-addIcon(iconSize - renWidth / (mult * pageIcons),(iconSize * ((iconAmount % pageIcons)+1)) / 1.07 - iconSize * 1 + (renWidth / (mult * pageIcons)),(iconSize * ((Math.floor(iconAmount / pageIcons))+1)) / 1.07 - iconSize * 1.02 + (renWidth / (mult * pageIcons)))
-addIcon(iconSize - renWidth / (mult * pageIcons),(iconSize * ((iconAmount % pageIcons)+1)) / 1.07 - iconSize * 1 + (renWidth / (mult * pageIcons)),(iconSize * ((Math.floor(iconAmount / pageIcons))+1)) / 1.07 - iconSize * 1.02 + (renWidth / (mult * pageIcons)))
+addIcon(iconSize - renWidth / (mult * pageIcons), (iconSize * ((iconAmount % pageIcons) + 1)) / 1.07 - iconSize * 1 + (renWidth / (mult * pageIcons)), (iconSize * ((Math.floor(iconAmount / pageIcons)) + 1)) / 1.07 - iconSize * 1.02 + (renWidth / (mult * pageIcons)), 'black', 'white')
+addIcon(iconSize - renWidth / (mult * pageIcons), (iconSize * ((iconAmount % pageIcons) + 1)) / 1.07 - iconSize * 1 + (renWidth / (mult * pageIcons)), (iconSize * ((Math.floor(iconAmount / pageIcons)) + 1)) / 1.07 - iconSize * 1.02 + (renWidth / (mult * pageIcons)), 'white', 'black')
+addIcon(iconSize - renWidth / (mult * pageIcons), (iconSize * ((iconAmount % pageIcons) + 1)) / 1.07 - iconSize * 1 + (renWidth / (mult * pageIcons)), (iconSize * ((Math.floor(iconAmount / pageIcons)) + 1)) / 1.07 - iconSize * 1.02 + (renWidth / (mult * pageIcons)), 'black', 'white')
+addIcon(iconSize - renWidth / (mult * pageIcons), (iconSize * ((iconAmount % pageIcons) + 1)) / 1.07 - iconSize * 1 + (renWidth / (mult * pageIcons)), (iconSize * ((Math.floor(iconAmount / pageIcons)) + 1)) / 1.07 - iconSize * 1.02 + (renWidth / (mult * pageIcons)))
+addIcon(iconSize - renWidth / (mult * pageIcons), (iconSize * ((iconAmount % pageIcons) + 2)) / 1.07 - iconSize * 1 + (renWidth / (mult * pageIcons)), (iconSize * ((Math.floor(iconAmount / pageIcons)) + 1)) / 1.07 - iconSize * 1.02 + (renWidth / (mult * pageIcons)))
 
-function addIcon(w, l, t) {
+function addIcon(w, l, t, fc, sc) {
     elements.push({
         width: w,
         height: w,
@@ -175,11 +176,11 @@ function addIcon(w, l, t) {
     })
 
     icons.push({
-        width: w,
-        height: w,
         top: t,
         left: l,
-        type: "icon"
+        type: "icon",
+        fc: fc,
+        sc: sc
     })
     iconAmount++
 }
